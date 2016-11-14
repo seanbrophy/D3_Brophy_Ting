f41ecffde1c4146b28b30b0ae34242b6d9011126
seanbrophy <sean.brophy22@outlook.com>
2016-11-14 12:13:00 -0500

    Empty Template
    adding empty template to repository

---
+++ js/main.js
@@ -0,0 +1,12 @@
+//Javascript Document
+
+(function(){
+	"use strict";//make it impossible to accidentally create global variables
+	
+	console.log("SEAF fired");
+	//variables
+
+	//functions
+
+	//listeners
+})();
