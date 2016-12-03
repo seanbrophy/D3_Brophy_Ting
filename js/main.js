5bab9ce8a4d01ba8333bd8022c94d5e2a1295f2c
seanbrophy <sean.brophy22@outlook.com>
2016-12-03 12:10:02 -0500

    json line graph
    doing a test with json data into line graph

--- js/main.js
+++ js/main.js
@@ -4,9 +4,30 @@
 	"use strict";//make it impossible to accidentally create global variables
 	
 	console.log("SEAF fired");
-	//variables
 
-	//functions
+	d3.json("data.json", function(data) {
+	   var canvas = d3.select("#graph").append("svg")
+	        .attr("width", 500)
+	        .attr("height", 500)
+	        .attr("border", "black")
 
-	//listeners
+	   var group = canvas.append("g")
+	        .attr("transform", "translate(100,10)")
+
+	   var line = d3.line()
+	        .x(function(d, i) {
+	            return d.x;
+	        })
+	        .y(function(d, i) {
+	            return d.y;
+	        }); 
+
+	   group.selectAll("path")
+	        .data(data).enter()
+	        .append("path")
+	        .attr("d", function(d){ return line(d) })
+	        .attr("fill", "none")
+	        .attr("stroke", "green")
+	        .attr("stroke-width", 3);
+	});
 })();
