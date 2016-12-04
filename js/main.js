3054a1d51c86ddf846eb7b6fc4499796fa846597
seanbrophy <sean.brophy22@outlook.com>
2016-12-04 12:50:33 -0500

    comments
    commented beginning portion of key points

--- js/main.js
+++ js/main.js
@@ -6,17 +6,19 @@
 	console.log("SEAF fired");
 	var width = 650;
     var height = 500;
+    //grabs a d3 color library
 	var color = d3.scaleOrdinal(d3.schemeCategory10);
 
+//grabs the data from a json file and creates a function using the data
 	d3.json("data.json", function(data) {
+		//creates the svg object to draw on after selecting an element with an id of 'graph'
 	   var canvas = d3.select("#graph").append("svg")
 	        .attr("width", width)
-	        .attr("height", height)
-	        .attr("border", "black")
+	        .attr("height", height);
 
 	   var group = canvas.append("g")
 	       
-
+	   //puts a line using data from a json file in a object to be drawn
 	   var line = d3.line()
 	        .x(function(d, i) {
 	            return d.x;
@@ -29,8 +31,10 @@
 	        .data(data)
 	        .enter()
 	        .append("path")
+	        //this next line draws the path using the line variable and the data
 	        .attr("d", function(d){ return line(d) })
 	        .attr("fill", "none")
+	        //supposed to give random color to each line, doesn't work
 	        .attr("stroke", color)
 	        .attr("stroke-width", 3);
 	});
