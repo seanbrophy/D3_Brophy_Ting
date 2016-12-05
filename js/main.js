b8ac0d8fbccc0636073eafca0c87dc1d59b5ec65
Sean Brophy <sean.brophy22@outlook.com>
2016-12-04 21:48:34 -0500

    axis
    starting graph over with axis in place

--- js/main.js
+++ js/main.js
@@ -4,38 +4,49 @@
 	"use strict";//make it impossible to accidentally create global variables
 	
 	console.log("SEAF fired");
-	var width = 650;
+	var width = 1000;
     var height = 500;
+    var padding = 30;
+ 
     //grabs a d3 color library
 	var color = d3.scaleOrdinal(d3.schemeCategory10);
 
 //grabs the data from a json file and creates a function using the data
 	d3.json("data.json", function(data) {
+
+		var xScale = d3.scaleLinear()
+						.range([padding, width - padding])
+						.domain([1,7]);
+
+		var yScale = d3.scaleLinear()
+						.range([height - padding, padding])
+						.domain([0,20]);
+
+		var xAxis = d3.axisBottom()
+	    			.scale(xScale)
+	    			.ticks(5); 
+
+		var yAxis = d3.axisLeft()
+	    			.scale(yScale)
+	    			.ticks(5); 
+
 		//creates the svg object to draw on after selecting an element with an id of 'graph'
 	   var canvas = d3.select("#graph").append("svg")
 	        .attr("width", width)
 	        .attr("height", height);
 
 	   var group = canvas.append("g")
-	       
-	   //puts a line using data from a json file in a object to be drawn
-	   var line = d3.line()
-	        .x(function(d, i) {
-	            return d.x;
-	        })
-	        .y(function(d, i) {
-	            return d.y;
-	        }); 
 
-	   group.selectAll("path")
-	        .data(data)
-	        .enter()
-	        .append("path")
-	        //this next line draws the path using the line variable and the data
-	        .attr("d", function(d){ return line(d) })
-	        .attr("fill", "none")
-	        //supposed to give random color to each line, doesn't work
-	        .attr("stroke", color)
-	        .attr("stroke-width", 3);
+	   group.append("g")
+			.attr("class", "axisX")
+			.attr("transform", "translate(0," + (height - padding) + ")")//sets the axis bar at the bottom
+	    	.call(xAxis);
+
+	   group.append("g")
+			.attr("class", "axisY")
+			.attr("transform", "translate(" + padding + ",0)")
+	    	.call(yAxis);
+	       
+	   
 	});
 })();
